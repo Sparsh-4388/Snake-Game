e9aca9b9622b932dcfc963a4a4769c319c44cf50
Sparsh Pal <sparshp773@gmail.com>
2025-07-01 07:42:16 +0530

    Initialized the controls

--- Python_Game.py
+++ Python_Game.py
@@ -4,7 +4,7 @@ import sys
 pygame.init()
 
 screen = pygame.display.set_mode((600,600))
-pygame.display.set_caption("My First Pygame Window")
+pygame.display.set_caption("Snake.io")
 
 clock = pygame.time.Clock()
 
@@ -12,6 +12,18 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+
+        #The Controls for the game
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP or event.key == pygame.K_w:
+                print("Up")
+            elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                print("Down")
+            elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                print("Left")
+            elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                print("Right")
+
 
     
     screen.fill((0,0,0))
